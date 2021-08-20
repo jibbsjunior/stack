4318130a9668fe6f74d9b6ae51a1b21d0a022545
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-08-20 11:24:52 -0700

    initial commit

---
+++ dist/stack.js
@@ -0,0 +1,59 @@
+"use strict";
+class Stack {
+    constructor() {
+        this.data = [];
+        this.top = 0;
+    }
+    push(element) {
+        if (this.data.includes(element))
+            return console.log("Sorry! this elements currently exist on stack!");
+        if (this.length() >= 10) {
+            return console.log("Sorry! this stack is currently full and can\'t take in new elements!");
+        }
+        else {
+            this.data[this.top] = element;
+            this.top++;
+        }
+    }
+    length() {
+        return this.top;
+    }
+    peek() {
+        return this.data[this.top - 1];
+    }
+    isEmpty() {
+        return this.top === 0;
+    }
+    isFull() {
+        if (this.length() === 10) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    reverse() {
+        this._reverse(this.top - 1);
+    }
+    _reverse(index) {
+        if (index != 0)
+            this._reverse(index - 1);
+        console.log(this.data[index]);
+    }
+    pop() {
+        if (this.isEmpty() === false) {
+            this.top = this.top - 1;
+            return this.data.pop();
+        }
+        else {
+            return console.log("Whooops! the stack is currently empty and there's no element to remove!!");
+        }
+    }
+    print() {
+        let top = this.top - 1;
+        while (top >= 0) {
+            console.log(this.data[top]);
+            top--;
+        }
+    }
+}
